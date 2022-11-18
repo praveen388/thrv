7c005455cc9804050a4fcd6dc2417b26cb499df8
praveen388 <webdeveloperd824@gmail.com>
2022-11-18 17:56:20 +0530

    Initial commit

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,63 @@
+<footer id="site-footer">
+<div class="footer-body">
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-xl-10 col-xl-offset-1">
+<div class="col-sm-4 col-md-3 footer-quick-links">
+<h4><i class="icon-link"></i> Quick Links</h4>
+<ul class="footer-nav">
+<li><a href="/">Home</a></li>
+<li><a href="/new-rvs-for-sale">New RVs</a></li>
+<li><a href="/used-rvs-for-sale">Used RVs</a></li>
+<li><a href="/rv-financing">Get Financing</a></li>
+<li><a href="/consign-your-rv">Consignments</a></li>
+<li><a href="/rv-specials">Specials</a></li>
+<li><a href="/rv-parts">Parts</a></li>
+<li><a href="/rv-service">Service</a></li>
+<li><a href="/about-us">About Us</a></li>
+<li><a href="/contact-us">Contact Us</a></li>
+</ul>
+</div>
+<div class="col-sm-4 col-md-3 footer-locations">
+<h4><i class="icon-map-marker"></i> Visit Us</h4>
+<p><strong>Trailer Hitch RV Sales</strong><br>215 N. Frontage Road<br>Nipomo, CA 93444<br>1-800-847-2989<br><a href="/driving-directions" target="_blank" rel="noopener">Directions</a></p>
+<p><strong>Trailer Hitch RV Service</strong><br>545 Sandydale Drive<br>Nipomo, CA 93444<br>1-<span>805-344-5108</span><br><a href="/driving-directions" target="_blank" rel="noopener">Directions</a></p>
+<p><strong>Trailer Hitch RV World Parts</strong><br>1530 West Grand Avenue<br>Grover Beach, CA 93433<br>1-<span>805-215-1824</span><br><a href="/driving-directions" target="_blank" rel="noopener">Directions</a></p>
+</div>
+<div class="col-sm-4 col-md-3 footer-hours">
+<h4><i class="icon-time"></i> Our Hours</h4>
+<div class="operating-hours">
+<p><strong>Sales</strong><br/>Monday - Saturday: 9:00 am - 5:00 pm<br/>Sunday: 10:00 am - 4:00 pm</p>
+<p><strong>Service</strong><br/>Monday - Saturday: 9:00 am - 5:00 pm<br/>Closed Sunday</p>
+<p><strong>Parts</strong><br/>RV PARTS - RV WORLD Ltd.<br/>(Grover Beach)<br/>Monday - Saturday: 9:00 am - 5:00 pm<br/>Sunday: 10:00 am - 2:00 pm</p>
+</div>
+</div>
+<div class="col-sm-12 col-md-3 footer-social">
+<h4><i class="icon-thumbs-up"></i> Connect Wth Us</h4>
+
+
+<ul class="social-icons">
+<li><a target="_blank" href="https://www.facebook.com/TrailerHitchRVCenter/" rel="noopener"><i class="icon-facebook" aria-hidden="true"></i><span>Facebook</span></a></li>
+<li><a target="_blank" href="https://www.youtube.com/channel/UCnL2HTHHvJvlRJOmcojRUMg" rel="noopener"><i class="icon-youtube" aria-hidden="true"></i><span>YouTube</span></a></li>
+<li><a target="_blank" href="https://twitter.com/home" rel="noopener"><i class="icon-twitter" aria-hidden="true"></i><span>Twitter</span></a></li>
+<li><a target="_blank" href="https://www.google.com/maps/place/Trailer+Hitch+RV/@35.0415788,-120.4922999,17z/data=!4m5!3m4!1s0x80ec6866cbb95a01:0xb9a51c3b8f5e5a13!8m2!3d35.039551!4d-120.488509" rel="noopener"><i class="icon-map-marker" aria-hidden="true"></i><span>Google Business Page</span></a></li>
+<li><a target="_blank" href="https://www.instagram.com/trailerhitchrv/" rel="noopener"><i class="icon-instagram" aria-hidden="true"></i><span>Instagram</span></a></li>
+</ul>
+<a href="/blog" class="btn btn-primary btn-lg btn-footer-blog">Our Blog</a><br><br><br><img src="{{ asset('assets/images/logo-bbb.png') }}" alt="BBB"></div>
+</div>
+</div>
+</div>
+</div>
+<div class="footer-bottom">
+<div class="container">
+<ul>
+<li>Copyright © 2022 Trailer Hitch RV<span>•</span></li>
+<li><a href="/privacy-policy">Privacy Policy</a><span>•</span></li>
+<li><span><a href="/sold-rvs">Sold RVs</a> •</span></li>
+<li><a href="http://www.interactrv.com" target="_blank" rel="noopener">RV Dealer Website Design</a></li>
+</ul>
+</div>
+</div>
+</footer><!-- [if lte IE 8]>
+<p class="browseHappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,94 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Album example for Bootstrap</title>
+<link href="https://assets-cdn.interactcp.com/interactrv/css/common/ds-global.css?version=1.61.1103.32319" type="text/css" rel="stylesheet"/>
+    
+    <!--[if (!IE) | (gte IE 8)]><!-->
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:400,700,900i|Teko:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="//assets.interactcp.com/interactrv,_css,_common,_font-awesome.min.css+trailerhitchrv,_css,_default.css+trailerhitchrv,_css,_client.css.pagespeed.cc.VRfaFk0IEe.css"/>
+    <!-- Favicon icon -->
+    <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
+    <!-- vendor css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"> 
+
+
+    <script src="https://assets-cdn-interactrv.netdna-ssl.com/interactrv/js/common_netcore/modernizr-2.7.2.js.pagespeed.jm.FoA8VhmRfo.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
+<link rel="icon" type="image/ico" href="https://assets.interactcp.com/trailerhitchrv/images/favicon.ico"/>
+<meta name="msvalidate.01" content="E8E324B97011A5AA53AE9D5431F9190C"/>
+
+<meta name="google-site-verification" content="KJWlg1t_Vpm78X18ERwjzVw0GNovpcBCLOBks_quKNc"/>
+<meta name="google-site-verification" content="AOrmOp3WfgLHklGcfLMN6g5OhcN-WvdpnA3vP76qhmk"/>
+<meta name="google-site-verification" content="sNLciHJhdUOp0sc-2Krtq-OrbUeL72DWpPUqixrQSks"/>
+<meta name="google-site-verification" content="1CSXPAgEIpnjbKN5WjSKtc4cjomSfEQu3LW_6ApLYHQ"/>
+<meta name="facebook-domain-verification" content="a89ujrmcrh639q4lvpifm0j4jf86xt"/>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-976949652"></script>
+
+<!-- Kenect ga event track-->
+<script>
+window.addEventListener('message', (event) => {
+ const eventType = event && event.data && event.data.type
+ switch(eventType) {
+  case 'EVENT_SEND_MESSAGE':
+   ga('send', {
+     hitType: 'event',
+     eventCategory: 'KenectWidget',
+     eventAction: 'send_message'
+   });
+   break
+  default:
+   break;
+  }
+})  
+</script>
+<!-- End Kenect ga event track -->
+<!-- Gtag Contact Snippet-->
+<script>var enhanced_conversion_data={};</script>
+<!-- Global site tag (gtag.js) - Google Ads: 976949652 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-976949652"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','AW-976949652',{'allow_enhanced_conversions':true});</script>
+<script src="https://assets.interactcp.com/trailerhitchrv/js/trailerhitchrv-redesign_com/homescripts.js.pagespeed.jm.Tc5TDVLTuc.js" type="text/javascript"></script>
+<script type="application/ld+json">
+{
+    "@context": "http://schema.org",
+    "@type": "Organization",
+    "name": "Trailer Hitch RV",
+    "additionalType": "https://en.wikipedia.org/wiki/Recreational_vehicle",
+    "naics": "441210",
+    "telephone": "805-929-8095",
+    "areaServed": "https://en.wikipedia.org/wiki/United_States",
+        "address": { 
+        "@type": "PostalAddress",
+        "addressCountry": "US", 
+        "addressLocality": "Nipomo",  
+        "addressRegion": "CA", 
+        "postalCode": "93444", 
+        "streetAddress": "215 N. Frontage Road" 
+    },
+    "description": "Shop a huge inventory of new &amp; used RVs online at Trailer Hitch RV, a trusted RV dealer in California. Search travel trailers, toy haulers &amp; fifth wheels.",
+    "logo": {
+        "@type": "ImageObject",
+        "name": "Trailer Hitch RV",
+        "url": "https://assets-cdn-interactrv.netdna-ssl.com//trailerhitchrv/images/logo.png",
+        "@id": "https://assets-cdn-interactrv.netdna-ssl.com//trailerhitchrv/images/logo.png"
+    },
+    "url": "https://www.trailerhitchrv.com", 
+"@id": "https://www.trailerhitchrv.com"
+}
+</script>
+
+    
+    <script type="text/javascript" src="https://assets-cdn-interactrv.netdna-ssl.com/interactrv/js/common_netcore/jquery.supplemental.js.pagespeed.jm.GY3vh0-lNR.js"></script>
+<script type="text/javascript" src="https://assets.interactcp.com/trailerhitchrv/js/site.js.pagespeed.jm.OJeuwJ4llX.js"></script>
+
+    
+    <!-- For ContactAtOnce chat referrer tracking -->
+    <script type="text/javascript">$(function(){if(window.lpTag!==undefined&&window.lpTag.vars!==undefined){lpTag.vars.push([{'name':'referrer','value':'trailerhitchrv.com'}]);}});</script>
+
+<script type="text/javascript">(function(){var regEx=/(?:(?:(?:(?:\d{1,2})(?:[-\.\s]))?)?(?:(?:\((?:\d{3})\))|(?:\d{3}))(?:[-\.\s])(?:\d{3})(?:[-\.\s])(?:\d{4}))|(tel:\d{10})/g;var regExParts=["(?:(?:(?:(?:\\d{1,2})(?:[-\\.\\s]))?)?(?:(?:\\((?:",")\\))|(?:","))(?:[-\\.\\s])(?:",")(?:[-\\.\\s])(?:","))|(tel:",")"];var escapeRegExp=function(str){return str.replace(/([.*+?^${}()|\[\]\/\\])/g,"\\$1");};$.fn.getPhoneNumbers=function(){var numbers=[];$(this).find(":not(iframe):not(input):not(select):not(textarea):not(.no-phone-tracking)").contents().filter(function(){return this.nodeType!=1;}).each(function(){var matches=$(this).text().match(regEx);if(matches!==null){for(var i=0;i<matches.length;i++){var match=matches[i];if(match.indexOf('tel:')===0){match=match.substring(4);}if(numbers.indexOf(match)===-1){numbers.push(match);}}}});return numbers;};var specificTrackingNumbers=[];var phoneTrackingApplied=false;var setByLeadTracking=false;$.fn.setPhoneTrackingNumber=function(number,filter,leadTracking){if(setByLeadTracking&&!leadTracking){return;}if(number!==undefined&&number!==''){console.log("Phone Tracking: Tracking number set. New Number: "+number+"; Filter: "+filter+"; Lead Tracking: "+leadTracking);specificTrackingNumbers.push({newNum:number,filter:filter});if(phoneTrackingApplied===true){applyPhoneTracking($(document),number,filter);}}};var applyPhoneTracking=function($el,number,filter){var find;if(filter===undefined||filter===null||filter===''){console.log('Phone Tracking: Applying phone tracking: '+number);find=regEx;}else{var phoneFilter=filter;if(filter.indexOf('{')===0){var filterObj=JSON.parse(filter);phoneFilter=filterObj.phone;}var phoneDigits=phoneFilter.replace(/\D+/g,'');if(phoneDigits.length===10){console.log('Phone Tracking: Replacing "'+filter+'" with "'+number+'"');var customRegex=regExParts[0]+phoneDigits.substring(0,3)+regExParts[1]+phoneDigits.substring(0,3)+regExParts[2]+phoneDigits.substring(3,6)+regExParts[3]+phoneDigits.substring(6,10)+regExParts[4]+phoneDigits+regExParts[5];find=new RegExp(customRegex,"g");}else{console.log('Phone Tracking: Replacing "'+filter+'" with "'+number+'"');find=new RegExp(escapeRegExp(filter),"g");}}var regExNumberReplacer=function(val){return val.indexOf('tel:')===0?'tel:'+number.replace(/-/g,''):number;};$el.find(":not(iframe):not(input):not(select):not(textarea):not(.no-phone-tracking)").contents().filter(function(){return this.nodeType!=1;}).each(function(){var $this=$(this);var output=$this.text().replace(find,regExNumberReplacer);if(output!=$this.text()){var parent=$this.parent('a');$this.wrap("<p></p>").parent('p').html(output).contents().unwrap();if(parent.length){if(parent.attr("href")!==undefined){parent.attr("href",parent.attr("href").replace(find,regExNumberReplacer));}if(parent.attr("title")!==undefined){parent.attr("title",parent.attr("title").replace(find,regExNumberReplacer));}};}});};$.fn.applyPhoneTracking=function(){var $this=$(this);for(var i=0;i<specificTrackingNumbers.length;i++){applyPhoneTracking($this,specificTrackingNumbers[i].newNum,specificTrackingNumbers[i].filter);}return this;};$(function(){$(document).applyPhoneTracking();phoneTrackingApplied=true;console.log("Phone Tracking: $(document).applyPhoneTracking() was called");});})();</script>
+    <script type="text/javascript">$(function(){$('a[href^="https://forms.interactcp.com"],a[href^="https://devforms.interactcp.com"],a[href^="https://testforms.interactcp.com"],a[href^="http://trailerhitchrv-redesign.interactrv.com"],a[href^="https://trailerhitchrv-redesign.interactrv.com"]').each(function(){var $link=$(this);var url=$link.attr('href');var qsParams=getUrlVars(url)||[];var qIndex=url.indexOf('?');var baseUrl=qIndex!==-1?url.substring(0,qIndex):url;if(qsParams.indexOf('iwsReferrer')===-1)qsParams.push('iwsReferrer');if(qsParams.indexOf('iwsReferrerQS')===-1)qsParams.push('iwsReferrerQS');qsParams['iwsReferrer']='trailerhitchrv.com';qsParams['iwsReferrerQS']='';var newQS=$.param($.map(qsParams,function(x){return{'name':x,'value':qsParams[x]};}));if(newQS.length)newQS='?'+newQS;$link.attr('href',baseUrl+newQS);});});</script>
+
